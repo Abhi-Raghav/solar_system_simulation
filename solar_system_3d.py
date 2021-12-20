d17fb29a9a0f07428792dd5ea4a6d29646912bac
Abhishek <abhishek@jaist.ac.jp>
2021-12-20 17:34:06 +0900

    Added SolarSystemBody class.

--- solar_system_3d.py
+++ solar_system_3d.py
@@ -1,6 +1,7 @@
 # solar_system_3d.py
 
 import matplotlib.pyplot as plt
+from vectors import Vector
 
 class SolarSystem:
     def __init__(self, size):
@@ -17,3 +18,17 @@ class SolarSystem:
 
     def add_body(self, body):
         self.bodies.append(body)
+
+
+class SolarSystemBody:
+    def __init__(self, solar_system, mass, position=(0, 0, 0), velocity=(0, 0, 0)):
+        self.solar_system = solar_system
+        self.mass = mass
+        self.position = position
+        self.velocity = Vector(*velocity)
+        self.solar_system.add_body(self)
+
+    def move(self):
+        self.position = (
+            self.position
+        )
